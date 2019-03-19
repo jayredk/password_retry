51d59a096a25599ce6cebae1e053ee96fa39e2f0
Jay <kred110396@gmail.com>
2019-03-19 23:24:51 +0800

    Update version 2

--- password.py
+++ password.py
@@ -1,12 +1,19 @@
-password = 123456
+# 密碼重試程式
+# password - '123456'
+# 讓使用者重複輸入密碼
+# 最多輸入3次
+# 輸入正確 則印出 “登入成功！”
+# 輸入錯誤 則印出 ”密碼錯誤！還有＿次機會“	
+
+password = '123456'
 count = 3
-while True:
+while count > 0:
 	x = input('請輸入密碼： ')
-	if int(x) == 123456:
+	count = count - 1
+	if x == password:
 		print('登入成功!')
 		break
 	else:
-		count = count - 1
 		if count == 0:
 			print('密碼錯誤！ 帳號鎖定15分鐘')
 			break
